8d7b0537a4cb2b11a13232b57bd2756bc6be6794
Hugo Marroquin <149089365+humarroquin@users.noreply.github.com>
2024-11-17 16:45:59 -0600

    added slider functionality

---
+++ main.js
@@ -0,0 +1,32 @@
+const sliderContainer = document.querySelector('.work__slider');
+const btnPrev = document.querySelector('.btn-prev');
+const btnNxt = document.querySelector('.btn-next');
+const images = document.querySelectorAll('.slider-img');
+
+btnNxt.addEventListener('click', () => {
+  sliderContainer.style.scrollBehavior = 'smooth';
+  sliderContainer.scrollLeft += 570;
+});
+
+btnPrev.addEventListener('click', () => {
+  sliderContainer.style.scrollBehavior = 'smooth';
+  sliderContainer.scrollLeft -= 570;
+});
+
+// Center the middle image
+const centerMiddleImage = () => {
+  const middleIndex = Math.floor(images.length / 2);
+  const middleImage = images[middleIndex];
+
+  // Calculate position to center the middle image
+  const offset =
+    middleImage.offsetLeft -
+    sliderContainer.clientWidth / 2 +
+    middleImage.clientWidth / 2;
+
+  // Set scroll position to the calculated offset
+  sliderContainer.scrollLeft = offset;
+};
+
+// Call the function once the page loads
+window.addEventListener('load', centerMiddleImage);
